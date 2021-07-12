05f24dcc7e6ae2be65e59eb979a877a9a7413c07
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-12 17:29:13 +0700

    Add usage comment

--- JavaScript/narze.js
+++ JavaScript/narze.js
@@ -1,1 +1,3 @@
+// Usage `node JavaScript/narze.js`, or run in browser console.
+
 console.log([...new Set("Coffee".split(''))].join('').replace("f", "d"))
--- Ruby/narze.rb
+++ Ruby/narze.rb
@@ -1,1 +1,3 @@
+# Usage : ruby Ruby/narze.rb
+
 puts "Coffee".tr(?f,?d).chars.uniq*""
