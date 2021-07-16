aa555c3e52572376a5d2fcba9de48ad0d16c8b74
Tinnaphat Somsang <68820538+TinPluss@users.noreply.github.com>
2021-07-16 15:13:10 +0700

    Create TinPluss.java

---
+++ Java/TinPluss.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TinPluss
+{
+public static void main(String args[]) 
+  {
+  String[] Coffee = "Coffee".split("");
+  List<String> CoffeeList = Arrays.asList(Coffee);
+  Set<String> set = new LinkedHashSet<>(CoffeeList);
+  String CoffeeString = String.join("", set);
+  String Code = CoffeeString.replace("f","d");
+  System.out.println(Code);
+  }
+}
