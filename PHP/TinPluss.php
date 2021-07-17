c12ef689f2a5f428e72de0cf0ae486ad0e318538
Tinnaphat Somsang <68820538+TinPluss@users.noreply.github.com>
2021-07-17 09:35:26 +0700

    Delete PHP directory

--- PHP/TinPluss.php
+++
@@ -1,7 +0,0 @@
-<!-- Usage : Run PHP on XAMPP ( More info at https://www.apachefriends.org/ )  -->
-
-<?php
-$Codee = str_replace("ff", "d", "Coffee");
-$Code = str_replace("ee","e", $Codee);
-echo $Code;
-?> 
