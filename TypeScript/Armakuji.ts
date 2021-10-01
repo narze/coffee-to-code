6f0090aa78022bdeff5ba7dc7393b8212f640d80
Armakuji <armakuji@hotmail.com>
2021-10-02 01:21:24 +0700

    fix(ConvertCoffeeTypeScript): fix wording function name

--- TypeScript/Armakuji.ts
+++ TypeScript/Armakuji.ts
@@ -1,9 +1,9 @@
-const convetCoffeeToCode = (str: string): string => {
+const convertCoffeeToCode = (str: string): string => {
   return str.split(/Coffee/i).join("Code");
 };
 
 const normalString = "Coffee is awesome!. Coffee makes me awake. I love Coffee";
 const advanceString = "CoffeeIsAwesomeCoffeeMakeMeAwakesILoveCoffee";
 
-console.log("normal convert : ", convetCoffeeToCode(normalString));
-console.log("advanced convert : ", convetCoffeeToCode(advanceString));
+console.log("normal convert : ", convertCoffeeToCode(normalString));
+console.log("advanced convert : ", convertCoffeeToCode(advanceString));
