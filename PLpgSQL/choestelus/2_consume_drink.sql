e7bde4dcec09d04cde2f82c5464445d2466367b0
Nattapong Amornbunchornvej <nattapong@chochoe.net>
2021-10-05 16:55:00 +0700

    Add PL/pgSQL examples

---
+++ PLpgSQL/choestelus/2_consume_drink.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.drink_entities (name, kind) VALUES ('java', 'coffee');
+INSERT INTO public.drink_entities (name, kind) VALUES ('oolong', 'tea');
+INSERT INTO public.drink_entities (name, kind) VALUES ('matcha', 'tea');
+INSERT INTO public.drink_entities (name, kind) VALUES ('mocha', 'coffee');
