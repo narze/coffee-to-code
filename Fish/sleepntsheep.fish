85dafb53052d4e559dc328aa60108d34a49f72f8
sleepntsheep <r4yish@gmail.com>
2021-10-05 19:33:10 +0700

    Remove old folder

--- Fish/sleepntsheep.fish
+++
@@ -1,8 +0,0 @@
-## screenshot: https://file.papangkorn.com/get/8vThoq/WindowsTerminal_VglWsV28Q4.png
-
-#!/bin/fish
-set coffee coffee
-set coffee (string replace ff d $coffee)
-set coffee (string replace ee e $coffee)
-echo $coffee
-
