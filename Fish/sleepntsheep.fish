311d3377a563bac0d813909ad253f6c5f56d9d7a
sleepntsheep <r4yish@gmail.com>
2021-10-05 19:28:12 +0700

    Fish

---
+++ Fish/sleepntsheep.fish
@@ -0,0 +1,8 @@
+## screenshot: https://file.papangkorn.com/get/8vThoq/WindowsTerminal_VglWsV28Q4.png
+
+#!/bin/fish
+set coffee coffee
+set coffee (string replace ff d $coffee)
+set coffee (string replace ee e $coffee)
+echo $coffee
+
