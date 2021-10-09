9e2729efecad9e0960f95c599c7a7b0614c67c77
satish <imsat0036@gmail.com>
2021-10-09 11:03:02 +0530

    Python code for draw Virus image

---
+++ Python/virusimagemaker.py
@@ -0,0 +1,21 @@
+import turtle
+
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor("black")
+t.pencolor("blue")
+a=0
+b=0
+t.speed()
+t.penup()
+t.goto(0,200)
+t.pendown()
+
+while True:
+    t.forward(a)
+    t.right(b)
+    a+=3
+    b+=1
+    if(b==210):
+        break
+    t.hideturtle()
