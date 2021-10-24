26b42126f012b4ce4df119563e213b2d52f2d470
Phanupong R <phanupong.ra@gmail.com>
2021-10-24 13:52:56 +0000

    add Python/phanupong.r

---
+++ Python/phanupongr.py
@@ -0,0 +1,11 @@
+coffee = input("Please type 'Coffee' ")
+while (coffee != "Coffee"):
+    coffee = input("Please type 'Coffee'")
+
+print("============================================")
+print("Converting Coffee to Code...................")
+print("============================================")
+
+code = coffee.replace("ffe", "d")
+
+print(code)
