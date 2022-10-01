c7b9854633ea788c5df09ac6255577179c3fcdd8
404Mate <cadenstolz05@Gmail.com>
2022-10-01 11:26:10 -0500

    Create 404Mate.py
    Coffee to code in python

---
+++ Python/404Mate.py
@@ -0,0 +1,14 @@
+# Coffee to Code
+
+drink = str.lower(input("What are you drinking?\n"))
+
+if drink == "java":
+    print("wrong language")
+elif drink == "coffee":
+    print(drink.replace("ff", "d")[:-1]) 
+else:
+    print("no output")
+
+#---------------------------
+#  Made with <3 by 404Mate  
+#--------------------------- 
