9bf7b510ff3906dbe0242f389c1827ad696afc8c
Sirati Hirunthani <49112474+BarwSirati@users.noreply.github.com>
2022-10-01 22:31:49 +0700

    Add files via upload

---
+++ C++/bxdman.cpp
@@ -0,0 +1,48 @@
+Last login: Sat Oct  1 15:01:36 on ttys000
+➜  ~ ls      
+Applications                 Tools
+Desktop                      Virtual Machines.localized
+Documents                    default-soapui-workspace.xml
+Downloads                    package-lock.json
+Library                      script.txt
+Movies                       smartbear.auth
+Music                        soapui-settings.xml
+Pictures                     sqlmap-dev
+Public
+➜  ~ cd Desktop/   
+➜  Desktop  
+➜  Desktop ls
+ScreenShot                           app
+Windows 10 x64 Image 110422.vmwarevm
+➜  Desktop micro bxdman.cpp
+
+
+
+
+
+
+
+ 1 #include<iostream>                                                           
+ 2 using namespace std;                                                         
+ 3 int main(){                                                                  
+ 4     string coffee;                                                           
+ 5     cin>>coffee;                                                             
+ 6     if(coffee == "coffee"){                                                  
+ 7         cout<<"code";                                                        
+ 8     }else{                                                                   
+ 9         cout<<"Run Program Again And Type coffee!!";                         
+10     }                                                                        
+11     return 0;                                                                
+12 }                                                                            
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+                                                                                
+bxdman.cpp + (11,11) | ft:c++ | unix | utf-8       Alt-g: bindings, Ctrl-g: help
+                                                                                
