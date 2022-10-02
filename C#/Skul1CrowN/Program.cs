538d000502d467f5183609c2dd6f4cff8ac96fce
Punnawit Wachirasethaherun <67839920+Skul1CrowN@users.noreply.github.com>
2022-10-02 23:46:02 +0700

    Add the other CSharp Project (#115)
    * Add the other CSharp Project
    
    * Remove unnessary files

---
+++ C#/Skul1CrowN/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main()
+    {
+        string? input = Console.ReadLine();
+        if(input == null)
+        {
+            Console.WriteLine("Something Went Wrong");
+        }
+        else
+        {
+            input = Regex.Replace(input,"coffee","code", RegexOptions.IgnoreCase);
+            Console.WriteLine(input);
+        }
+    }
+}
