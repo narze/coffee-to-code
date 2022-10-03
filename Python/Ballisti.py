f0f38cadcef2ed7e85d88c6fa84f50bc455300cd
Calum Breen-Chapman <78085467+Ballisti@users.noreply.github.com>
2022-10-03 12:34:35 +0100

    Ballisti (#136)

---
+++ Python/Ballisti.py
@@ -0,0 +1,9 @@
+coffee=""
+while coffee !="y":
+    coffee=input("consume coffee y/n ").lower()
+    if coffee=="y":
+        print("""coffee=""
+while coffee !="y":
+    coffee=input("consume coffee y/n ").lower()
+    if coffee=="y":
+        print("code")""")
