e474a1c133aa5db6b696718c49fd589e40246487
PeterPrombutr <61090023@kmitl.ac.th>
2022-10-06 13:27:19 +0700

    Add more coffee!
    this cup of coffee made from svelte beans

---
+++ Svelte/peterprombutr.svelte
@@ -0,0 +1,23 @@
+<script>
+	let cup = { cupEmpty: false };
+
+	function toggle() {
+		cup.cupEmpty = !cup.cupEmpty;
+	}
+</script>
+
+{#if cup.cupEmpty}
+	<h1>
+		Here's your code ( • ∀•)–Ψ
+	</h1>
+	<button on:click={toggle}>
+		Add more coffee?
+	</button>
+	
+{/if}
+
+{#if !cup.cupEmpty}
+	<button on:click={toggle}>
+		Drink your coffee ☕︎
+	</button>
+{/if}
