a23a413e21e63f185e568e25e4c2741cd63107ec
Manaswini123456 <janu2001dec@gmail.com>
2022-10-11 20:25:03 +0530

    ReactJS Coffee to Code

---
+++ reactjs/manaswini123456.js/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+
+function App() {
+  const [buttonText,setButtonText] = useState('Coffee');
+
+  function handleClick(){
+    if (buttonText==='Coffee'){
+      setButtonText('Code');
+    } else{
+      setButtonText('Coffee')
+    }
+    console.log(buttonText);
+    
+  }
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h1>REACT JS CHANGING BUTTON</h1>
+        <h2>Try Yourself and Enjoy!</h2>
+      <img src={logo} className="App-logo" alt='text'></img>
+      <button onClick={handleClick} className="doublechange">{buttonText}</button>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
