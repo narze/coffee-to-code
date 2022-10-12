2cc37bc0780e9f48353713f85b0a0363579599dc
Andrew Vittiglio <77115975+andr3wV@users.noreply.github.com>
2022-10-12 05:57:19 -0400

    add new Swift Language (#158)
    Co-authored-by: Andrew Vittiglio <andrew@andrews-air.dhcp.nd.edu>

---
+++ Swift/andr3wV.swift
@@ -0,0 +1,8 @@
+func coffeeToCode(input: String = "Nothing"){
+	print("You have \(input).");
+	if(input=="Coffee") { print("You have Code!") }
+	else { print("You have no Coffee!!") }
+}
+
+
+coffeeToCode(input: "Coffee")
