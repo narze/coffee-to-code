357614b0482aad67e968b2fbcf3b892cc23fa43c
negin.khademian <neginkhademian@gmail.com>
2022-10-31 21:01:09 +0330

    feat: add coffee to code converter for Vue2

---
+++ Vue/NeginKhademian.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="convertCoffeeToCode">{{ coffeeToCode }}</button>
+</template>
+
+<script>
+export default {
+  name: "convertCoffeeToCode",
+  data: () => ({
+    coffeeToCode: "i worship coffee",
+  }),
+  methods: {
+    convertCoffeeToCode() {
+      this.coffeeToCode = this.coffeeToCode.replace("coffee", "code");
+    },
+  },
+};
+</script>
