d461f8bc7ca526ce5e847f1521ca0aadb1472d6b
thititongumpun <thiti180536@gmail.com>
2023-10-02 20:10:29 +0700

    add C#/thititongumpun

---
+++ C#/thititongumpun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            string input = "";
+
+            while (input.ToLower() != "coffee")
+            {
+                Console.WriteLine("Please input Coffee or COFFEE: ");
+                input = Console.ReadLine()!;
+                if (input.ToLower() == "coffee")
+                {
+                    Console.WriteLine(ToTitleCase("code"));
+                }
+            }
+            Environment.Exit(0);
+        }
+    }
+
+    private static string ToTitleCase(string str)
+    {
+        var textInfo = new CultureInfo("en-US", false).TextInfo;
+        return textInfo.ToTitleCase(str);
+    }
+}
