32c2d06052c72145505bc2f575abdae7c78109b9
OfekSusan <ofeksushantrainers@gmail.com>
2023-11-24 18:43:40 +0200

    Create ofekSushan.cpp

---
+++ C++/ofekSushan.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    bool replace = false;
+    string coffee = "coffee";
+
+    for (int index = 0; index < coffee.size() - 1; index++)
+    {
+        if (!replace && coffee[index] == 'f')
+        {
+            coffee[index] = 'd';
+
+            replace = true;
+        }
+        else if (replace && coffee[index] == 'f' || coffee[index] == 'e')
+        {
+            coffee.erase(index, 1);
+            index--;
+        }
+    }
+
+    cout << coffee << endl;
+
+    return 0;
+}
